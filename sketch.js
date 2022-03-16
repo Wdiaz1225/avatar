9e859fb82d8c2bb9ab88b27e0a89eae6e40b748d
Wdiaz1225 <wilsitoj1@hotmail.com>
2022-03-16 13:17:34 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,48 +7,48 @@ function setup() {
   createCanvas(400, 400);
   background(50);
   angleMode(DEGREES);
-  
+
   circleY = 100;
 
   circleX = 120;
-  
-  
+
+
   mic = new p5.AudioIn();
   mic.start();
-   
+
 }
 
-
+//test
 
 function draw(){
   background(50);
-  
-  
+
+
   drawSpikes();
-  
+
   drawBody();
-  
+
   drawMouth();
-  
+
   movingMouth();
-  
+
   drawEyes();
-  
+
   drawSingleSpike();
 }
 
 function mousePressed(){
-   
+
   circleX = circleX + 50;
   circleY = circleY + 50;
-  
+
 }
 
 function mouseReleased(){
-  
+
   circleX = circleX - 50;
   circleY = circleY - 50;
-  
+
 }
 
 
@@ -58,7 +58,7 @@ function drawEyes(){
   noStroke();
   circle(width*.6,width*.4,circleX);
   circle(width*.78,width*.42,circleY);
-  
+
   //pupils
   fill(0);
 ellipse(width*.61,width*.4,width*.04,width*.09);
@@ -70,26 +70,26 @@ function drawSpikes(){
   stroke(0)
   strokeWeight(4)
   fill(150,20,20);
-  
+
   if (mouseX > 150){
     fill(0,0,200);
   }
-  
+
   triangle(width*.35,height*.23, width*.5,height*.04,width*.65, height*.23);
   triangle(width*.2,height*.45, width*.15,height*.2,width*.3, height*.23);
   triangle(width*.2,height*.65, width*.05,height*.55,width*.2, height*.45);
-  
+
 }
 
 function drawMouth(){
   //mouth
   fill(50);
   noStroke()
-  
+
 
 }
   //mouth
-function movingMouth(){    
+function movingMouth(){
   var vol = mic.getLevel();
   push();
   fill(2);
@@ -97,18 +97,18 @@ function movingMouth(){
   fill(2);
   pop();
 }
-  
+
 function drawBody(){
   //body
   strokeWeight(4);
   fill(150,20,20);
-  
+
   if (mouseX > 150){
     fill(0,0,200);
   }
-    
+
   circle(width*.5,width*.5,width*.7);
-  
+
 }
 
 function drawSingleSpike(){
@@ -118,5 +118,5 @@ function drawSingleSpike(){
   //triangle(width*.35,height*.57, width*.2,height*.4);(lines)
   line(width*.2,height*.4,width*.37,height*.35);
   line(width*.2,height*.4,width*.37,height*.5);
-  
+
 }
